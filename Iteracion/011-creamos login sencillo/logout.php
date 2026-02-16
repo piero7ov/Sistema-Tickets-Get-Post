09423a06057bdac523ae81cbb02502da5549e022
Piero Olivares <piero7ov@gmail.com>
2026-02-17 00:33:50 +0100

    Create logout.php

---
+++ Iteracion/011-creamos login sencillo/logout.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+/* Vaciar sesión */
+$_SESSION = [];
+
+/* Borrar cookie de sesión (si existe) */
+if (ini_get("session.use_cookies")) {
+  $p = session_get_cookie_params();
+  setcookie(session_name(), "", time() - 42000, $p["path"], $p["domain"], $p["secure"], $p["httponly"]);
+}
+
+session_destroy();
+
+header("Location: login.php");
+exit;
